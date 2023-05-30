db491f53d6782cbecf91c2dcefa63b0a12ec70a4
cheeze <maksprofy51@gmail.com>
2023-05-30 20:44:21 +0300

    added fonts to application

--- Timon.Maui/MauiProgram.cs
+++ Timon.Maui/MauiProgram.cs
@@ -11,8 +11,18 @@ public static class MauiProgram
             .UseMauiApp<App>()
             .ConfigureFonts(fonts =>
             {
+                fonts.AddFont("OpenSans-Bold.ttf", "OpenSansBold");
+                fonts.AddFont("OpenSans-BoldItalic.ttf", "OpenSansBoldItalic");
+                fonts.AddFont("OpenSans-ExtraBold.ttf", "OpenSansExtraBold");
+                fonts.AddFont("OpenSans-ExtraBoldItalic.ttf", "OpenSansExtraBoldItalic");
+                fonts.AddFont("OpenSans-Italic.ttf", "OpenSansItalic");
+                fonts.AddFont("OpenSans-Light.ttf", "OpenSansLight");
+                fonts.AddFont("OpenSans-LightItalic.ttf", "OpenSansLightItalic");
+                fonts.AddFont("OpenSans-Medium.ttf", "OpenSansMedium");
+                fonts.AddFont("OpenSans-MediumItalic.ttf", "OpenSansMediumItalic");
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("OpenSans-SemiBold.ttf", "OpenSansSemiBold");
+                fonts.AddFont("OpenSans-SemiBoldItalic.ttf", "OpenSansSemiBoldItalic");
             });
 
 #if DEBUG

731f44939b183568c718e5ff38fd6eb0b4b65bd4
cheeze <maksprofy51@gmail.com>
2023-05-30 20:08:08 +0300

    Add project files.

---
+++ Timon.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Timon.Maui;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Timon.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Timon.Maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
